5684c5ff7565c067ae37190109231f8f93d561b1
Lucas Santos <140768315+Lucass641@users.noreply.github.com>
2024-04-12 12:21:21 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,42 @@
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
-
-let menu = document.querySelector('.menu')
-let botaoMenu = document.querySelector('.btn-menu');
-
+let barraAnimada = document.querySelectorAll('.bar');
 
 botaoMostrarProjetos.addEventListener('click', () => {
     mostrarMaisProjetos();
     esconderBotao();
-    });
-function esconderBotao(){
-botaoMostrarProjetos.classList.add('remover');
+});
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add('remover');
 }
 
-function mostrarMaisProjetos(){
+function mostrarMaisProjetos() {
     projetosInativos.forEach(projetoInativo => {
         projetoInativo.classList.add('ativo');
     });
 }
 
+
+let menu = document.querySelector('.menu');
+let botaoMenu = document.querySelector('.btn-menu');
+
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
+    barraAnimada.forEach(barra =>{
+        barra.classList.toggle('animate');
+        
+        window.onscroll = () => {
+            menu.classList.remove('ativo');
+            barraAnimada.forEach(barra =>{
+                barra.classList.remove('animate')
+            });
+        };
+
+    });
+
 });
 
-window.onscroll = () => {
-    menu.classList.remove('ativo');
-};
+
+
 
 
